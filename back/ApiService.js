fb5b83bb4a223c523820df878f38ad54fa4ec8b3
PratikGarai <pratikgarai0208@gmail.com>
2021-03-25 20:17:41 +0530

    added data source

---
+++ back/ApiService.js
@@ -0,0 +1,24 @@
+const { RESTDataSource } = require('apollo-datasource-rest');
+
+class PeopleAPI extends RESTDataSource {
+    constructor () {
+        super();
+        this.baseUrl = "http://localhost:5000/";
+    }
+    
+    getAllPeople = async () => {
+        const response = await fetch(this.baseUrl+"people");
+        const res = await res.json();
+        console.log(res);
+        return res['data']['people'].map(person => this.personReducer(person));
+    }
+    
+    personReducer = (person) => {
+        return {
+            name : person.name, 
+            age : person.id
+        }
+    }
+}
+
+module.exports = PeopleAPI;
--- back/index.js
+++ back/index.js
@@ -1,7 +1,13 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
+const PeopleAPI = require('./ApiService.js');
 
-const server = new ApolloServer({ typeDefs });
+const server = new ApolloServer({ 
+    typeDefs,
+    dataSources : ()=> ({
+        peopleAPI : new PeopleAPI()
+    }) 
+});
 
 server.listen().then(() => {
     console.log("Server is running!");
